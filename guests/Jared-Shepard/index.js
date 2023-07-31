63c7c1d31b91bd1c7ccbaacf164b724e2888da87
dwpulsip <darren@pulsipher.org>
2023-07-31 10:16:46 -0700

    Adding subscribe to podcast to the briefs.md

---
+++ guests/Jared-Shepard/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "id": "Jared Shepard",
+    "name": "Jared Shepard",
+    "email": "",
+    "notes": "Notes",
+    "bio": "bio.md",
+    "thumbnail": "thumbnail.png",
+    "socials": {}
+}
